1f0495ff63e0af2b8449c87ba51c76ac012fdcd0
C. SHEN <chunhua@me.com>
2024-07-06 13:28:14 +0930

    CS: commit save

--- bibformat/cli.py
+++ bibformat/cli.py
@@ -9,6 +9,11 @@ def main(args=None):
     if len(sys.argv) > 1:
         bibf = sys.argv[1]
         # print("The input bibtex file is: " + str(bibf) )
+
+        print("% This bibtex is generated using https://github.com/cshen/bibformat   " +  x.strftime("%D %T"))
+        print("% You will need to: wget https://raw.githubusercontent.com/cshen/bibformat/main/cvml.bib")
+        print("")
+
         bf.format_bibtex( str(bibf) )
     else:
         print("Please input a bibtex file to process.")
